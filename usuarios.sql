9dc241c51a3f54995f921c6db563f1423331f82c
EduardoRoquue <43285951+EduardoRoquue@users.noreply.github.com>
2020-12-06 15:49:33 -0600

    Update usuarios.sql
    Se modificaron las variables

--- usuarios.sql
+++ usuarios.sql
@@ -3,12 +3,12 @@ CREATE DATABASE usuarios;
 
 /*CREATE TABLE*/
 CREATE TABLE 'usuarios' (
-    'ID_User' int(11) NOT NULL auto_increment,
-    'nombre' VARCHAR(32) collate utf8_unicode_ci NOT NULL,
-    'ap_pat' VARCHAR(32) collate utf8_unicode_ci NOT NULL,
-    'ap_mat' VARCHAR(32) collate utf8_unicode_ci NOT NULL,
-    'username' VARCHAR(32) collate utf8_unicode_ci NOT NULL DEFAULT,
-    'password' VARCHAR(32) collate utf8_unicode_ci NOT NULL DEFAULT,
+    ID_User int(11) NOT NULL auto_increment,
+    nombre VARCHAR(32) COLLATION utf8_unicode_ci NOT NULL,
+    ap_pat VARCHAR(32) collate utf8_unicode_ci NOT NULL,
+    ap_mat VARCHAR(32) collate utf8_unicode_ci NOT NULL,
+    username VARCHAR(32) collate utf8_unicode_ci NOT NULL DEFAULT,
+    password VARCHAR(32) collate utf8_unicode_ci NOT NULL DEFAULT,
     PRIMARY KEY('ID_User'),
     UNIQUE KEY 'username' ('username')
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8'COLLATE=utf8_unicode_ci';
