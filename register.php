1744e753783ff92ca43869d6e0e82a9ab0a68efd
EduardoRoquue <43285951+EduardoRoquue@users.noreply.github.com>
2020-12-06 04:55:31 -0600

    update register.php
    Se agregó el formulario de registro

--- register.php
+++ register.php
@@ -37,3 +37,44 @@ if(isset($_POST["register"]))
     }
 }
 ?>
+
+<?php if(!empty($message))
+{
+    echo "<p class=\"error\">" . "Mensaje:" . $message . "</p";
+}
+?>
+<div class="container mregister">
+<div id="login">
+<h1>Registrar</h1>
+<form name="registerform" id="registerform" action="register.php" method="POST">
+
+<p>
+<label for="user_login">Nombre<br/>
+<input type="text" name="nombre" id="nombre" class="input" size="32" value="" /></label>
+</p>
+
+<p>
+<label for="user_login">Apellido Paterno<br/>
+<input type="text" name="ap_pat" id="ap_pat" class="input" size="32" value="" /></label>
+</p>
+
+<p>
+<label for="user_login">Apellido Materno<br/>
+<input type="text" name="ap_mat" id="ap_mat" class="input" size="32" value="" /></label>
+</p>
+
+<p>
+<label for="user_pass">Nombre de usuario<br/>
+<input type="text" name="username" id="username" class="input" size="32" value="" /></label>
+</p>
+
+<p>
+<label for="user_pass">Contraseña<br/>
+<input type="password" name="password" id="password" class="input" size="32" value="" /></label>
+</p>
+
+</form>
+</div>
+</div>
+
+<?php include("includes/footer.php"); ?>
