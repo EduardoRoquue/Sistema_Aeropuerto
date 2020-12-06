312976537c34847ecba7ef6d2011aacb4058e387
EduardoRoquue <43285951+EduardoRoquue@users.noreply.github.com>
2020-12-06 03:01:37 -0600

    div class
    revisar toda la wea anterior

--- login.php
+++ login.php
@@ -21,6 +21,54 @@ if(isset($_POST["login"]))
         $password = $_POST['password'];
 //TODO: revisar esta wea
         $mysqli = new mysqli($host ="localhost", $username = "username", $password = "", $dbname = "usuarios");
-        $sql = SELECT ID 
+        $numrows=mys ql_num_rows(mysqli);
+        if($numrows!=0)
+        {
+            while($row=mysql_fetch_assoc($mysqli))
+            {
+                $dbusername=$row['username'];
+                $dbpassword=$row['password'];
+            }
+
+            if($username == $dbusername && $password == $dbpassword)
+            {
+                $_SESSION['session_username']=$username;
+                /*Redirect browser*/
+                header("Location: intropage.php");
+            }
+        } else {
+            $message = "Nombre o contraseña incorrectos";
+        }
+    } else {
+        $message = "Todos los campos requeridos";
     }
-}
+} //TODO: Revisar toda la wea anterior
+?>
+
+<div class="container mlogin">
+    <div id="login">
+        <h1>Logueo</h1>
+        <form name="loginform" id="loginform" action="" method="POST">
+            <p>
+                <label for="user_login">Nombre de usuario<br/>
+                <input type="text" name="username" id="username" class="input" value="" size="20"/>
+            </label>
+            </p>
+            <p>
+                <label for="user_pass">Contraseña<br />
+            <input type="password" name="password" id="password" class="input" value="" size="20" /> 
+        </label>
+            </p>
+            <p class="submit">
+                <input type="submit" name="login" class="button" value="Entrar"/>
+            </p> 
+            <p class="regtext">No estas registrado <a href="register.php">Registrate aqui</a>!</p>
+        </form>
+    </div>
+</div>
+<?php include("includes/footer.php"); ?>
+<?php if(!empty($message))
+{
+echo "<p class=\"error\">" . "MESSAGE: ".$message. "</p";
+} 
+?>
